2155c9d29a032a3a05e9e3ee7665cb8af8f84126
David Sor <david.sor@intusurg.com>
2026-03-18 13:34:01 -0400

    Initial Commit

---
+++ IDRApp/IDRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDRAppApp.swift
+//  IDRApp
+//
+//  Created by David Sor on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct IDRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
